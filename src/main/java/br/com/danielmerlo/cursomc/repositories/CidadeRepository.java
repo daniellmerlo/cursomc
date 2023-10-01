4cb031de3027bd3918516a5ec624e50460bc5ca5
Daniel Merlo <daniellmerlo@gmail.com>
2023-10-01 00:59:42 -0300

    Estado e cidade

---
+++ src/main/java/br/com/danielmerlo/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package br.com.danielmerlo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.danielmerlo.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+}
---
+++ src/main/java/br/com/danielmerlo/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package br.com.danielmerlo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.danielmerlo.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+}
